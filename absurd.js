f735e0aeca0b174b28d69a310cd401364cb7df9a
vincent <53187806+20100lefebvre@users.noreply.github.com>
2020-08-12 14:34:55 +0200

    Update absurd.js

--- absurd.js
+++ absurd.js
@@ -1,5 +1,5 @@
 	var sujet = ["Le roi du monde", "Elle", "Le chien", "Une casserole", "Bobby", "Le chat", "Le voisin", "Le président"];
-	var verbe = ["aime", "rencontre", "voit", "sauve", "pense à", "apprend","s'amuse avec","mange", "range", "glisse sur"];
+	var verbe = ["aime", "rencontre", "voit", "sauve", "pense à", "prend","s'amuse avec","mange", "range", "glisse sur"];
 	var complement = ["une maison", "une omelette", "une rayure", "une porte", "une cuisine", "une fille","une poule","une banane"];
 	var adjectif =["ridicule" , "baveuse", "orgueilleuse", "neuve", "blonde", "rouge", "moche", "jolie"];
 	var action = [ "et s'enfuit", "et ramasse un papier", "et lit le journal", "et accepte sa destinée", "et sauve le monde", "et s'endort", "et se lave les pieds"];
